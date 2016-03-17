879c5f9595190ea0df0300226d6e69090965e7ea
Shakeeb Sadikeen <shakee.zats@gmail.com>
2016-03-17 16:02:33 +0530

    Static init Used

--- demo.php
+++ demo.php
@@ -3,17 +3,16 @@
 	include_once("fonoapi-v1.php");
 
 	$apiKey = "xxxxxxxx"; // get your token key here - https://fonoapi.freshpixl.com
-	$fonoapi = new fonoApi($apiKey);
-
+	$fonoapi = fonoApi::init($apiKey);
 		try {
 
 			$res = $fonoapi::getDevice("i9305"); // the device you need to get details here
 
 			foreach ($res as $mobile) {
 				if (!empty($mobile->DeviceName)) 	echo "Device : ". $mobile->DeviceName . "<br>";
-				if (!empty($mobile->Brand)) 		  echo "Brand : ". $mobile->Brand . "<br>";
-				if (!empty($mobile->cpu)) 			  echo "Cpu : " . $mobile->cpu . "<br>";
-				if (!empty($mobile->status)) 		  echo "Status : " . $mobile->status . "<br>";
+				if (!empty($mobile->Brand)) 		echo "Brand : ". $mobile->Brand . "<br>";
+				if (!empty($mobile->cpu)) 			echo "Cpu : " . $mobile->cpu . "<br>";
+				if (!empty($mobile->status)) 		echo "Status : " . $mobile->status . "<br>";
 				if (!empty($mobile->dimensions)) 	echo "Dimensions : " . $mobile->dimensions . "<br>";
 				if (!empty($mobile->_4g_bands)) 	echo "4g : " .$mobile->_4g_bands . "<br>";
 			}
